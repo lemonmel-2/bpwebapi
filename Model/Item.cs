6fe1f95781be1da91100af45ac2c4aef4d3a6462
lemonmel-2 <maychin0205@gmail.com>
2025-12-07 03:01:05 +0800

    fix(item model): fix naming

--- Model/Item.cs
+++ Model/Item.cs
@@ -2,14 +2,14 @@ namespace webapi.Model
 {
 public class Item
     {
-        public string ItemID { get; set; }
+        public string ItemId { get; set; }
         public string Name { get; set; }
 
         public int Quantity { get; set; }
 
         public Item(string itemId, string name)
         {
-            ItemID = itemId;
+            ItemId = itemId;
             Name = name;
         }
     }
